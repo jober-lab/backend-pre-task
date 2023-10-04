968e0c7e629c9372503cf58782cc519d5f99a417
realslimtaek <89964264+realslimtaek@users.noreply.github.com>
2023-10-04 19:45:08 +0900

    feat : 캐리어 수정 및 추가 API 추가

--- backend/routes/career/index.js
+++ backend/routes/career/index.js
@@ -21,5 +21,37 @@ router.get('/:id', asyncWrapper(async (req, res) => {
 
 
 
+//캐리어 추가 및 수정
+router.post('/modify', asyncWrapper(async (req, res) => {
+
+    let car = await Career.findOne({
+        where: {
+            id: Number(req.body.id),
+            user_id: Number(req.body.user_id)
+        },
+    }).then(result => {
+        if (result) {
+            result.update({
+                company_name: req.body.company_name,
+                job: req.body.job,
+                join_date: req.body.join_date,
+                quit_date: req.body.quit_date,
+            });
+        } else {
+            Career.create({
+                company_name: req.body.company_name,
+                user_id : req.body.user_id,
+                job: req.body.job,
+                join_date: req.body.join_date,
+                quit_date: req.body.quit_date,
+            })
+        }
+        res.json({
+            ok: true,
+            code: 200,
+            message: "modified"
+        })
+    })
+}))
 
 module.exports = router;
