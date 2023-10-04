58565da9dba91396cf04a5b1b0d1bbc0fa0c46fb
realslimtaek <89964264+realslimtaek@users.noreply.github.com>
2023-10-04 18:04:28 +0900

    feat : profile_card 페이지네이션 처리 API 개발 완료.

--- backend/routes/profileCard/index.js
+++ backend/routes/profileCard/index.js
@@ -8,9 +8,43 @@ const bodyParser = require('body-parser');
 router.use(bodyParser.json());
 
 
+//기본 조회
+router.get('/', asyncWrapper(async (req, res) => {
+
+    let pageNum = req.query.pageNum; // 요청 페이지 넘버
+    let offset = 0;
+    let orderBy = req.query.orderBy;
+    let orderByStandard = req.query.orderByStandard;
+
+    if (pageNum > 1) {
+        offset = 10 * (pageNum - 1);
+    }
+
+    const list = await ProfileCard.findAll({
+        offset: offset,
+        limit: 10,
+        order: [[orderByStandard, orderBy]]
+    })
+        .then(async result => {
+            return res.status(200).json({
+                ok: true,
+                message: ("success"),
+                content: result
+            })
+        })
+        .catch(err => {
+            return res.status(400).json({
+                ok : false,
+                message:("failed"),
+                content: err
+            })
+        });
+
+}));
+
 
 //프로필 삭제
-router.delete("/profile-card/delete", asyncWrapper(async (req, res) => {
+router.delete("/delete", asyncWrapper(async (req, res) => {
 
     let user = await ProfileCard.findOne({
         where: {
@@ -21,8 +55,8 @@ router.delete("/profile-card/delete", asyncWrapper(async (req, res) => {
         if (result == null) {
             res.json({
                 ok: false,
-                code : 400,
-                content : "already deleted"
+                code: 400,
+                content: "already deleted"
             })
         } else {
             result.destroy();
