58e1bfb1277f9edfa79350ec43be66b725238269
realslimtaek <89964264+realslimtaek@users.noreply.github.com>
2023-10-04 16:18:05 +0900

    feat : profile_card 삭제 api 제작 완료.

--- backend/routes/profileCard/index.js
+++ backend/routes/profileCard/index.js
@@ -2,14 +2,41 @@ const express = require('express');
 const router = express.Router();
 
 const asyncWrapper = require('../../context/asyncWrapper');
-const { profile_card: ProfileCard } = require('../../models');
+const {profile_card: ProfileCard} = require('../../models');
+const {career: Career} = require('../../models')
+const bodyParser = require('body-parser');
+router.use(bodyParser.json());
 
-router.get('/', asyncWrapper(async (req, res) => {
-  const list = await ProfileCard.findAll();
 
-  res.json({
-    list,
-  });
-}));
+
+//프로필 삭제
+router.delete("/profile-card/delete", asyncWrapper(async (req, res) => {
+
+    let user = await ProfileCard.findOne({
+        where: {
+            id: Number(req.body.user_id),
+            deleted_at: null
+        }
+    }).then(result => {
+        if (result == null) {
+            res.json({
+                ok: false,
+                code : 400,
+                content : "already deleted"
+            })
+        } else {
+            result.destroy();
+            return "delete completed";
+        }
+    })
+
+    res.json(
+        {
+            ok: true,
+            content: user
+        }
+    );
+
+}))
 
 module.exports = router;
